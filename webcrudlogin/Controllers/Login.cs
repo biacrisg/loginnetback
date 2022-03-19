7a4b61f786fe48eb38852082a7a6719ea5df9808
biacrisg <beatrizcristina08@hotmail.com>
2022-03-18 23:43:06 -0300

    first

---
+++ webcrudlogin/Controllers/Login.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace webcrudlogin.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logar(string username, string senha)
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("server=database-base.cfj1bpto3ea6.us-east-1.rds.amazonaws.com; database= db_magalu; uid=biacrisg; password=Marialuiza4");
+            await mySqlConnection.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT * FROM  db_magalu.`db_magalu.usuarios` WHERE username = '{username}' AND password = '{senha}'";
+
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+           
+                if (await reader.ReadAsync())
+                {
+                    return Json(new { MSG = "Bem vindo ao magalu "});
+                }
+
+            
+
+            return Json(new { MSG = " Erro ao logar" });
+
+        }
+    }
+}
---
+++ webcrudlogin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webcrudlogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
